c5e083c48568bfe39da4b1d4aeaeb66935d09f4e
Sanjeev Kumar <snjkumar@amazon.com>
2023-10-12 17:36:16 +0530

    cosematic changes

--- src/components/EntityList.jsx
+++ src/components/EntityList.jsx
@@ -4,16 +4,18 @@ import { CLOUDFRONT_URL } from "../config"
 const EntityList = ({ results }) => {
   console.log(results)
 
-  const filtered = results.filter((ele, index) => results.findIndex(obj => obj.value == ele.value) === index)
+  const filtered = results.filter((ele, index) => results.findIndex(obj => obj.name == ele.name && obj._source.ts == ele._source.ts) === index)
 
-  console.log('filtered', filtered)
+  const generateDatabaseDateTime = (date) => {
+    return date.toISOString().replace("T", " ").substring(0, 19);
+  }
 
   return (
     <div>
       {filtered?.map((result) => {
         const video_url = result._source.videoClipName;
 
-        const absolute_video_url = CLOUDFRONT_URL + "/" + video_url
+        const absolute_video_url = 'https://' + CLOUDFRONT_URL + "/" + video_url
         const base_ts = result._source.base_timestamp
         const ts = result._source.ts
         const timestamp = new Date(parseInt(base_ts) + parseInt(ts))
@@ -25,9 +27,11 @@ const EntityList = ({ results }) => {
               <video width="300px" height="200px" controls>
                 <source src={`${absolute_video_url}`} type="video/mp4" />
               </video>
-              <section className="entity__info">
-                <div className="entity__name"><b>Entity Detected:</b> {result._source.name}</div>
-                <div className="entity__name"><b>Detection Time:</b> {timestamp.toISOString()}</div>
+              <section className="entity_info">
+                <div ><b>Entity Detected:</b> {result._source.name}</div>
+                <div ><b>Video Clip Start Time:</b> {generateDatabaseDateTime(new Date(base_ts))}</div>
+                <div ><b>Entity Detection Time:</b> {generateDatabaseDateTime(timestamp)}</div>
+                <div ><b>Entity Detected at {ts / 1000}th second in video</b> </div>
               </section>
             </div>
             <hr />
